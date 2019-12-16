8a02199e7a65b89b4acb7cfdded561a39d01798e
Emmanuel Nyachoke <enyachoke@gmail.com>
2019-12-16 16:47:24 +0300

    Add more cases

--- libs/suminator.js
+++ libs/suminator.js
@@ -5,13 +5,45 @@ Suminator.prototype.getSum = function (a, b) {
 };
 
 Suminator.prototype.getSumPlusOne = function (a, b) {
-  return a + b + 1;
+ 
+};
+
+Suminator.prototype.getSumPlusTwo = function (a, b) {
+  
+};
+
+Suminator.prototype.getSumPlusThree = function (a, b) {
+  
+};
+
+Suminator.prototype.getSumPlusFour = function (a, b) {
+  
+};
+
+Suminator.prototype.getSumPlusFive = function (a, b) {
+   
 };
 
 Suminator.prototype.getSumTimesTwo = function (a, b) {
-  const sum = a + b;
-
-  return sum * 2;
+  
 };
+
+Suminator.prototype.getSumTimesThree = function (a, b) {
+  
+};
+
+Suminator.prototype.getSumTimesFour = function (a, b) {
+  
+};
+
+Suminator.prototype.getSumTimesFive = function (a, b) {
+  
+};
+
+Suminator.prototype.getSumTimesSix = function (a, b) {
+  
+};
+
+
 
 module.exports = Suminator;
--- test/chai_assert_tests.js
+++ test/chai_assert_tests.js
@@ -28,21 +28,81 @@ describe('Mocha buildin assert', () => {
         assert.equal(suminator.getSum(2, -3), -1);
       });
     });
-    describe('getSumPlusOne', () => {
-      it('2 + 2 + 1 should return 5', () => {
-        assert.equal(suminator.getSumPlusOne(2, 2), 5);
-      });
-      it('2 + (-3) + 1 should return 0', () => {
-        assert.equal(suminator.getSumPlusOne(2, -3), 0);
-      });
-    });
-    describe('getSumTimesTwo', () => {
-      it('2 + 2 times 2 should return 8', () => {
-        assert.equal(suminator.getSumTimesTwo(2, 2), 8);
-      });
-      it('2 + (-3) + 1 should return -2', () => {
-        assert.equal(suminator.getSumTimesTwo(2, -3), -2);
-      });
-    });
+    // describe('getSumPlusOne', () => {
+    //   it('2 + 2 + 1 should return 5', () => {
+    //     assert.equal(suminator.getSumPlusOne(2, 2), 5);
+    //   });
+    //   it('2 + (-3) + 1 should return 0', () => {
+    //     assert.equal(suminator.getSumPlusOne(2, -3), 0);
+    //   });
+    // });
+
+    // describe('getSumPlusTwo', () => {
+    //   it('2 + 2 + 2 should return 6', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, 2), 6);
+    //   });
+    //   it('2 + (-4) + 2 should return 0', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, -4), 0);
+    //   });
+    // });
+
+    // describe('getSumPlusThree', () => {
+    //   it('2 + 2 + 3 should return 7', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, 2), 7);
+    //   });
+    //   it('2 + (-5) + 3 should return 0', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, -5), 0);
+    //   });
+    // });
+
+    // describe('getSumPlusFour', () => {
+    //   it('2 + 2 + 4 should return 8', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, 2), 7);
+    //   });
+    //   it('2 + (-6) + 4 should return 0', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, -6), 0);
+    //   });
+    // });
+
+    // describe('getSumPlusFive', () => {
+    //   it('2 + 2 + 5 should return 9', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, 2), 7);
+    //   });
+    //   it('2 + (-7) + 5 should return 0', () => {
+    //     assert.equal(suminator.getSumPlusTwo(2, -7), 0);
+    //   });
+    // });
+
+
+    // describe('getSumTimesTwo', () => {
+    //   it('2 + 2 times 2 should return 8', () => {
+    //     assert.equal(suminator.getSumTimesTwo(2, 2), 8);
+    //   });
+    // });
+
+    // describe('getSumTimesThree', () => {
+    //   it('2 + 2 times 2 should return 12', () => {
+    //     assert.equal(suminator.getSumTimesThree(2, 2), 12);
+    //   });
+    // });
+
+    // describe('getSumTimesFour', () => {
+    //   it('2 + 2 times 4 should return 16', () => {
+    //     assert.equal(suminator.getSumTimesFour(2, 2), 16);
+    //   });
+    // });
+
+    // describe('getSumTimesFive', () => {
+    //   it('2 + 2 times 5 should return 20', () => {
+    //     assert.equal(suminator.getSumTimesFive(2, 2), 20);
+    //   });
+    // });
+
+    // describe('getSumTimesSix', () => {
+    //   it('2 + 2 times 6 should return 24', () => {
+    //     assert.equal(suminator.getSumTimesSix(2, 2), 24);
+    //   });
+    // });
+
   });
 });
